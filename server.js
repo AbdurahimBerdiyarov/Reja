b3316fe2f41e2aa83932827b41036a8c029e1b4e
AbdurahimBerdiyarov <hoorwen@icloud.com>
2023-01-06 17:39:47 +0900

    callback function and F-task

--- server.js
+++ server.js
@@ -1,44 +1,74 @@
 console.log("web Serverni boshlash");
-
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
-const http = require("http");
-const fs = require("fs");
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
+console.log("Jack Ma maslahatlari");
+const list = [
+  "yaxshi talaba boling", // 0-20
+  "togri boshliq tanlang va koproq hato qiling", //  20-30
+  "ozingizga ishlashni boshlang", // 30-40
+  "siz kuchli bolgan narsalarni qiling", // 40-50
+  "yoshlarga investitsiya qiling", // 50-60
+  "endi dam oling, foydasi yoq endi", // 60
+];
+function maslahatBering(a, callback) {
+  if (typeof a !== "number") callback("insert a number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    setTimeout(function () {
+      callback(null, list[5]);
+    }, 5000);
+  }
+}
+console.log("passed here 0");
+maslahatBering(65, (err, data) => {
+  if (err) console.log("ERROR:", err);
+  else {
+    console.log("javob", data);
   }
 });
-// 1 Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+console.log("passed here 1");
 
-// 2 Session code
+// const express = require("express");
+// const res = require("express/lib/response");
+// const app = express();
+// const http = require("http");
+// const fs = require("fs");
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//   if (err) {
+//     console.log("ERROR:", err);
+//   } else {
+//     user = JSON.parse(data);
+//   }
+// });
+// // 1 Kirish code
+// app.use(express.static("public"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// 3 Views
-app.set("views", "views");
-app.set("view engine", "ejs");
+// // 2 Session code
 
-// 4 Routing
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
+// // 3 Views
+// app.set("views", "views");
+// app.set("view engine", "ejs");
 
-app.get("/develop", (req, res) => {
-  res.render("develop", { user: user });
-});
-app.get("/", function (req, res) {
-  res.render("harid");
-});
+// // 4 Routing
+// app.post("/create-item", (req, res) => {
+//   console.log(req.body);
+//   res.json({ test: "success" });
+// });
 
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is runing successfully on port: ${PORT}`);
-});
+// app.get("/develop", (req, res) => {
+//   res.render("develop", { user: user });
+// });
+// app.get("/", function (req, res) {
+//   res.render("harid");
+// });
+
+// const server = http.createServer(app);
+// let PORT = 3000;
+// server.listen(PORT, function () {
+//   console.log(`The server is runing successfully on port: ${PORT}`);
+// });
--- train.js
+++ train.js
@@ -1,19 +1,97 @@
+console.log("tran ishga tush");
+
+function findDoubler(str) {
+  let new_string = str.split("");
+
+  let war = new_string.map((val, index) => {
+    if (val == new_string[index + 1]) return true;
+    else return false;
+  });
+
+  return war.includes(true);
+}
+const result = findDoubler("hello");
+console.log(result);
+
 // const e = require("express");
 
-// console.log("tran ishga tush");
+// const func = (a, b, c) => {
+//   if (b == "/" && c == 0) {
+//     return "not valid to operate'";
+//   } else if (b == "+") {
+//     return a + c;
+//   } else if (b == "-") {
+//     return a - c;
+//   } else if (b == "*") {
+//     return a * c;
+//   } else if (b == "/") {
+//     return a / c;
+//   }
+// };
+// const war = func(6, "/", 0);
+// console.log(war);
 
-const func = (a, b, c) => {
-  if (b == "/" && c == 0) {
-    return "not valid to operate'";
-  } else if (b == "+") {
-    return a + c;
-  } else if (b == "-") {
-    return a - c;
-  } else if (b == "*") {
-    return a * c;
-  } else if (b == "/") {
-    return a / c;
-  }
-};
-const war = func(6, "/", 0);
-console.log(war);
+// function findDoubler(str) {
+//   if (typeof str === "str") {
+//     return "string ";
+//   }
+//   return str
+//     .split("")
+//     .map((val) => val.repeat(2))
+//     .join("");
+// }
+
+// const result = findDoubler("rahim");
+// console.log(result);
+
+// function findDoubler(str) {
+//   if (
+//     str
+//       .split("")
+//       .map((val) => val.repeat(2))
+//       .join("")
+//   ) {
+//     return str;
+
+//   } else if (
+//     str
+//       .split("")
+//       .map((val) => val.repeat(1))
+//       .join("")
+//   ) {
+//     return false;
+//   }
+// }
+
+// const result = findDoubler("rahhim");
+// console.log(result);
+
+// function findDoubler(str) {
+//   const new_string = str.split("");
+//   console.log(new_string);
+//   // const img = new_string.repeat(2);
+//   // console.log(img);
+
+//   const war = new_string.map((val) => {
+//     if (val.repeat(2)) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+//   return war.join("");
+
+//   // if (typeof str === "str") {
+//   //   return "string ";
+//   // }
+//   // // const war = str
+//   //   .split("")
+//   //   .map((val) => val.repeat(2))
+//   //   .join("");
+//   // return war;
+// }
+
+// const result = findDoubler("rahim");
+// console.log(result);
+
+// const war = new_string.map((val) => {
